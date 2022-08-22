004d662083a102d18b6c9ba7fe9c0925b9d13267
Mati <matiasnguarnieri@hotmail.com>
2022-08-22 18:06:23 -0300

    tercer commit, agregué parametros al constru

--- src/progra2/pruebaGit.java
+++ src/progra2/pruebaGit.java
@@ -5,7 +5,8 @@ public class pruebaGit {
 	private Integer numeroX;
 	private Integer numeroY;
 	
-	public pruebaGit () {
-		
+	public pruebaGit (Integer numeroX, Integer numeroY) {
+		this.numeroX = numeroX;
+		this.numeroY = numeroY;
 	}
 }
